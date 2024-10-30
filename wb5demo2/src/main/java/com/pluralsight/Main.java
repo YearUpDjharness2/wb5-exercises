82c979897fa770fdcefdcd00cf123ab32b7acfe1
Djharness <djharness2@gmail.com>
2024-10-30 10:50:10 -0400

    add

---
+++ wb5demo2/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+            Human h = new Human();
+            h.eat();
+            Caveman c = new Caveman();
+            c.eat();
+            ModernHuman m = new ModernHuman();
+            m.eat();
+        }
+    }
---
+++ wb5demo2/src/main/java/com/pluralsight/ModernHuman.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class ModernHuman extends Human{
+    public void eat() {
+        System.out.println("Me going to McDonalds and eat!");
+    }
+}
